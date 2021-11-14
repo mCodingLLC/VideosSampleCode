122edfbf5a805930bb031dad584e79a6c487ba7a
James Murphy <james@mcoding.io>
2021-11-14 10:52:45 -0600

    add 25 nooby habits

---
+++ videos/083_signs_youre_a_python_noob/depending_on_directory_structure.py
@@ -0,0 +1,9 @@
+from mypackage.nearby_module import awesome_function
+
+
+def main():
+    awesome_function()
+
+
+if __name__ == '__main__':
+    main()
---
+++ videos/083_signs_youre_a_python_noob/import_star.py
@@ -0,0 +1,5 @@
+from itertools import *
+
+# who knows what variables are in scope now?
+
+count()
